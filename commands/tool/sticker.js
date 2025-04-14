0e79cbca6776d949670f0b8c29118b3ca02f2464
riezz <rekberariez@gmail.com>
2025-04-14 16:54:25 +0700

    sticker

--- commands/tool/sticker.js
+++ commands/tool/sticker.js
@@ -1,9 +1,10 @@
 const { MessageType } = require("@mengkodingan/ckptw");
-const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
-
+const { Sticker, StickerTypes } = require('wa-sticker-formatter')
+const fs = require('fs')
 module.exports = {
-    name: ['sticker','s'],
-    type: 'command',
+    name: 'sticker',
+    aliases: ["sticker","s"],
+    command: 'hears',
     code: async (ctx) => {
         let m = ctx._msg;
         let command = m.content.split(' ')[0];
@@ -13,9 +14,9 @@ module.exports = {
             let image;
             try {
                 if (ctx.getMessageType() === MessageType.extendedTextMessage) {
-                    image = await ctx.quoted.media
+                    image = await ctx.quoted.media.toBuffer()
                 } else if (ctx.getMessageType() === MessageType.imageMessage) {
-                    image = await ctx.msg.media
+                    image = await ctx.msg.media.toBuffer()
                 } else {
                     return false;
                 }
@@ -24,21 +25,29 @@ module.exports = {
                 await ctx.react(ctx.id, '⛔');
             }
 
-            const buffer = await new Sticker(image)
-                .setPack('My Sticker')
-                .setAuthor('Yrizzz')
-                .setType(StickerTypes.DEFAULT)
-                .setCategories(['🤩', '🎉'])
-                .setId('12345')
-                .setBackground('#000000')
-                .setQuality(100)
-                .toBuffer()
-
-            await ctx.reply({ sticker: buffer }, { ephemeralExpiration: m?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 });
+            fs.writeFileSync('./tmp-s.jpeg', image);
+            const sticker = new Sticker('./tmp-s.jpeg', {
+                pack: 'My Pack', // The pack name
+                author: 'Yrizzz', // The author name
+                type: 'full', // The sticker type
+                categories: [ '🎉'], // The sticker category
+                id: ctx.id, // The sticker id
+                quality: 50, // The quality of the output file
+                background: '#000000' // The sticker background color (only for full stickers)
+            })
+            let msg = await sticker.toMessage()
+            console.log(msg)
+            await ctx.reply( msg , { ephemeralExpiration: m?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 });
             await ctx.react(ctx.id, '✅');
-
+            fs.unlink('./tmp-s.jpeg', (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('File is deleted.');
+                }
+            });
         } catch (err) {
-            console.log(err);
+            console.log(err)
             await ctx.react(ctx.id, '⛔');
             await ctx.reply({ text: 'internal server error' }, { ephemeralExpiration: m?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 });
         }
