cf2ba9c88cccb6fd764180f68e70550b643806cd
riezz <rekberariez@gmail.com>
2025-04-19 13:35:28 +0700

    add translite

--- commands/tool/translite.js
+++ commands/tool/translite.js
@@ -12,19 +12,19 @@ module.exports = {
         let data = m.content.slice(command.length + 1);
         await ctx.react(ctx.id, '⏳')
         try {
-            console.log( m.content )
             data = helper.filtermessage(m, data);
 
             let formdata = new FormData();
             formdata.append('from', 'auto');
             formdata.append('to', m.content.split(' ')[1]);
-            formdata.append('q', data);
+            formdata.append('q', data.split(' ')[1]);
 
             let result = await req('POST', `https://inter.youdao.com/translate`, formdata,{
                 headers: {
                     'Content-Type': `multipart/form-data; boundary=----WebKitFormBoundaryGBO2NOB2Ox3iMBMS`,
                 }
             })
+            
             result = result.data
             let replyMsg = '';
             replyMsg += '✅ Translated successfully\n';
