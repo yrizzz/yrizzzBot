788bd5a659547ac73072a303a0d52e4049b74d67
riezz <rekberariez@gmail.com>
2025-06-10 10:00:27 +0700

    add who

--- commands/tool/who.js
+++ commands/tool/who.js
@@ -8,8 +8,9 @@ module.exports = {
         let data = m.content.slice(command.length + 1);
         await ctx.react(ctx.id, '⏳');
         try {
-            const sender = data ?? m.key.participant ?? m?.key.remoteJid;
-            console.log(sender);
+
+            const sender = data ?? m?.key?.participant ?? m?.key.remoteJid;
+            console.log(sender,data);
             const result = await req('GET', `https://yrizzz.my.id/api/v1/tool/phoneChecker?phone=${sender}`);
             await ctx.reply({ text: result.data }, { ephemeralExpiration: m?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 });
             await ctx.react(ctx.id, '✅');
